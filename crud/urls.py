bf903113afc31639aba29b3307b7174c76762362
damodara <jay108@mail.ru>
2026-05-19 14:01:36 +0300

    начало подготовки базового шаблона

--- crud/urls.py
+++ crud/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from crud import views
 
 urlpatterns = [
-    path("", views.read, name="read"),
+    path("", views.index, name="index"),
     path("create/", views.create, name="create"),
     path("update/<int:rtmp_id>/", views.update, name="update"),
     path("delete/<int:rtmp_id>/", views.delete, name="delete"),
--- crud/views.py
+++ crud/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from crud.models import Rtmp
+
+# получение данных из бд
+def index(request):
+    rtmp = Rtmp.objects.all()
+    return render(request, "crud/index.html", {"rtmps": rtmp})
 
 
 def create(request):
     return HttpResponse("create page")
 
 
-def read(request):
-    return HttpResponse("read page")
+def read(request, rtmp_id):
+        template = loader.get_template("crud/index.html")
+        context = {"rtmp_id": rtmp_id}
+        return render(request, template, context)
 
 
 def update(request, rtmp_id):
